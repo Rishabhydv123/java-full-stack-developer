112adfcd4649426b4c7ee2be895743407855796b
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-10-29 12:07:45 +0530

    update

---
+++ JavaScript/Data-Type.js
@@ -0,0 +1,24 @@
+// format
+
+
+// "name"=> string
+// 00 => number
+// true / false => boolen
+// [cs,pubg,coc] <= array/collection
+// {name,age,address,contact} <= object/documents
+// undefine => undefine
+// null => null
+// big-IN => 9.255555555544
+
+
+
+// [{},{},{}] <= collection of document's mongodb
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
