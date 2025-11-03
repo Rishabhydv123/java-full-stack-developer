49bc77f3d6a7bb1bbfe94455d6381c1aced98ec8
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-03 12:26:57 +0530

    update

--- JavaScript/js/If-Statements.js
+++ JavaScript/js/If-Statements.js
@@ -11,23 +11,72 @@
 // }
 
 // Q1
-let number = 10; 
+// let number = 10; 
 
-if (number % 2 === 0) {
-  console.log(number + " is even");
-} else {
-  console.log(number + " is odd");
-}
+// if (number % 2 === 0) {
+//   console.log(number + " is even");
+// } else {
+//   console.log(number + " is odd");
+// }
 
 // Q2
 
-let num = 1
-if (num > 1) {
-    console.log("The number is positive");
-} else if (num < -1) {
-    console.log("The number is negative");
-} else {
-    console.log("The number is zero");
-}
+// let num = -10
+
+// if (num > 1) {
+//     console.log("The number is positive");
+// } else if (num < -1) {
+//     console.log("The number is negative");
+// } else {
+//     console.log("The number is zero");
+// }
 
 // Q3
+// let a1 = 10;
+// let b2 = 25;
+// if (a1 > b2) {
+//   console.log("The greater number is: " + a1);
+// } else {
+//   console.log("The greater number is: " + b2);
+// }
+
+// Q4
+// let a = 10;
+// let b = 25;
+// let c = 15;
+
+// if (a >= b && a >= c) {
+//   console.log("The largest number is: " + a);
+// }
+//  else if (b >= a && b >= c) {
+//   console.log("The largest number is: " + b);
+// }
+//  else {
+//   console.log("The largest number is: " + c);
+// }
+
+// Q5
+let score = 85; 
+let grade;
+
+if (score >= 90 && score <= 100) {
+  grade = "A";
+} else if (score >= 80 && score <= 89) {
+  grade = "B";
+} else if (score >= 70 && score <= 79) {
+  grade = "C";
+} else if (score >= 60 && score <= 69) {
+  grade = "D";
+} else if (score < 60) {
+  grade = "F";
+} else {
+  grade = "Invalid score";
+}
+console.log("Grade:", grade);
+
+
+
+
+
+
+
