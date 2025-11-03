af52a4ca0c8184f9efa7527145e6a27628ed1fb2
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-03 23:01:56 +0530

    update

--- JavaScript/js/If-Statements.js
+++ JavaScript/js/If-Statements.js
@@ -1,16 +1,16 @@
-// let AGE = 75;
+let AGE = 75;
 
-// if(AGE >=18 && AGE<56){
-//     console.log("Eligible for vote");  
-// }
-// else if(AGE>=56 && AGE<75){
-//     console.log("Null for vote");
-// }
-// else{
-//     console.log("Not eligible fot vote");
-// }
+if(AGE >=18 && AGE<56){
+    console.log("Eligible for vote");  
+}
+else if(AGE>=56 && AGE<75){
+    console.log("Null for vote");
+}
+else{
+    console.log("Not eligible fot vote");
+}
 
-// Q1
+// Q1. Check if a number is even or odd.
 // let number = 10; 
 
 // if (number % 2 === 0) {
@@ -19,7 +19,7 @@
 //   console.log(number + " is odd");
 // }
 
-// Q2
+// Q2 Check if a number is positive, negative, or zero.
 
 // let num = -10
 
@@ -31,7 +31,7 @@
 //     console.log("The number is zero");
 // }
 
-// Q3
+// Q3 Find the greater between two numbers
 // let a1 = 10;
 // let b2 = 25;
 // if (a1 > b2) {
@@ -40,7 +40,7 @@
 //   console.log("The greater number is: " + b2);
 // }
 
-// Q4
+// Q4 Find the largest among three numbers.
 // let a = 10;
 // let b = 25;
 // let c = 15;
@@ -55,27 +55,95 @@
 //   console.log("The largest number is: " + c);
 // }
 
-// Q5
-let score = 85; 
-let grade;
+// Q5.Check if a given year is a leap year or not.
 
-if (score >= 90 && score <= 100) {
-  grade = "A";
-} else if (score >= 80 && score <= 89) {
-  grade = "B";
-} else if (score >= 70 && score <= 79) {
-  grade = "C";
-} else if (score >= 60 && score <= 69) {
-  grade = "D";
-} else if (score < 60) {
-  grade = "F";
+//  let year =2025;
+// if (year % 4 === 0 && year % 100 !== 0) {
+//   console.log(year + " is a leap year.");
+// } else if (year % 400 === 0) {
+//   console.log(year + " is a leap year.");
+// } else {
+//   console.log(year + " is not a leap year.");
+// }
+
+
+// Q6 A student’s score is given. Print the grade based on:
+// 90–100 → A
+// 80–89 → B
+// 70–79 → C
+// 60–69 → D
+// below 60 → F
+
+// let score = 85; 
+// let grade;
+
+// if (score >= 90 && score <= 100) {
+//   grade = "A";
+// } else if (score >= 80 && score <= 89) {
+//   grade = "B";
+// } else if (score >= 70 && score <= 79) {
+//   grade = "C";
+// } else if (score >= 60 && score <= 69) {
+//   grade = "D";
+// } else if (score < 60) {
+//   grade = "F";
+// } else {
+//   grade = "Invalid score";
+// }
+// console.log("Grade:", grade);
+
+// Q7 Categorize a person by age:
+// 0–12 → Child
+// 13–19 → Teen
+// 20–59 → Adult
+// 60+ → Senior
+// let age = 20;
+
+//   if (age < 13) {
+//     console.log("Child");
+//   } else if (age < 18) {
+//     console.log("Teenager");
+//   } else if (age < 60) {
+//     console.log("Adult");
+//   } else {
+//     console.log("Senior");
+//   } 
+
+
+// Q8 Check if a given character is a vowel or consonant.
+let char = 'a';
+
+if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+  console.log("Vowel");
 } else {
-  grade = "Invalid score";
+  console.log("Consonant");
 }
-console.log("Grade:", grade);
 
 
+// Q9 Check if a number is divisible by both 3 and 5.
 
+// let number = 20; 
+
+// if (number % 3 === 0 && number % 5 === 0) {
+//     console.log("The number is divisible by both 3 and 5.");
+// } else {
+//     console.log("The number is not divisible by both 3 and 5.");
+// }
+
+
+// Q10 A temperature is given. Print:
+// ≤ 10°C → Cold
+// 11–25°C → Pleasant
+// >= 25°C → Hot
+let temperature = 30;
+
+if (temperature > 25) {
+  console.log("Hot");
+} else if (temperature < 15) {
+  console.log("Cold");
+} else {
+  console.log("Warm");
+}
 
 
 
--- JavaScript/js/Switch,Loops.js
+++ JavaScript/js/Switch,Loops.js
@@ -1,27 +1,24 @@
-// let AGE = 100;
+let AGE = 100;
 
-// switch (true) {
-//     case AGE >= 18 && AGE < 56:
-//         console.log('Eligible for vote');
-//         break;
-//          case AGE >= 56 && AGE < 75:
-//         console.log('Null for vote');
-//         break;
-//          case AGE >= 18:
-//         console.log(' Not Eligible for vote');
-//         break;
-//     default:
-//         console.log('key was not matched');
-//         break;
-// }
+switch (true) {
+    case AGE >= 18 && AGE < 56:
+        console.log('Eligible for vote');
+        break;
+         case AGE >= 56 && AGE < 75:
+        console.log('Null for vote');
+        break;
+         case AGE >= 18:
+        console.log(' Not Eligible for vote');
+        break;
+    default:
+        console.log('key was not matched');
+        break;
+}
   
 
 // Q1.Take a number (1–7). Print the day of the week.
 
-const readline = require("readline-sync");
-
-let day = parseInt(readline.question("Enter a number (1-7): "));
-
+let day = 5
 switch (day) {
   case 1:
     console.log("Sunday");
@@ -49,9 +46,177 @@ switch (day) {
 }
 
 
-// Loop
-let arr = [1,2,3];
-for(var i = 0; i < arr.length; i++){
-    console.log(arr[i]);
-    
+
+//  Q2 Take a month number (1–12). Print the month name.
+let month=8;
+
+switch (month){
+    case 1:
+        console.log("jan");
+        break;
+        case 2:
+        console.log("Feb");
+        break;
+        case 3:
+        console.log("march");
+        break;
+        case 4:
+        console.log("April");
+        break;
+        case 5:
+        console.log("May");
+        break;
+        case 6:
+        console.log("June");
+        break;
+        case 7:
+        console.log("july");
+        break;
+        case 8:
+        console.log("August");
+        break;
+        case 9:
+        console.log("sept");
+        break;
+        case 10:
+        console.log("Oct");
+        break;
+        case 11:
+        console.log("Nov");
+        break;case 12:
+        console.log("Dec");
+        break;
+        default:
+            console.log(null);
 }
+
+
+//Q3 Input a number (1–4). Print:
+// 1 → Addition
+// 2 → Subtraction
+// 3 → Multiplication
+// 4 → Division
+
+let num=4;
+switch (num){
+    case 1:
+        console.log("Adition");
+        break;
+        case 2:
+        console.log("subtraction");
+        break;
+        case 3:
+        console.log("Multiplicatin");
+        break;
+        case 4:
+        console.log("Division");
+        break;
+        default:
+            console.log(null);
+}
+
+// Q4 Input a grade (A, B, C, D, F). Print a message like Excellent, Good, etc.
+let grade = 'B';
+switch (grade) {
+  case "A":
+    console.log("Excellent");
+    break;
+  case "B":
+    console.log("Very Good");
+    break;
+  case "C":
+    console.log("Good");
+    break;
+  case "D":
+    console.log("Nice");
+    break;
+  case "F":
+    console.log("Poor");
+    break;
+  default:
+    console.log("Invalid grade");
+}
+
+
+// Q5 Input the first letter of a traffic light (R, Y, G). Print Stop, Ready, or Go.
+let light = 'G'; 
+switch (light) {
+  case 'R':
+    console.log("Stop");
+    break;
+  case 'Y':
+    console.log("Ready");
+    break;
+  case 'G':
+    console.log("Go");
+    break;
+  default:
+    console.log("Invalid input");
+}
+
+
+// Q6 Input a browser name (Chrome, Firefox, Edge, Safari). Show a launch message.
+let browser = 'chrome';  
+switch (browser) {
+  case 'chrome':
+    console.log("Launch Chrome...");
+    break;
+  case 'firefox':
+    console.log("Launch Firefox...");
+    break;
+  case 'edge':
+    console.log("Launch Edge...");
+    break;
+  case 'safari':
+    console.log("Launch Safari...");
+    break;
+  default:
+    console.log("null browser!");
+}
+
+
+
+// Q7 Input a number (1–3). Print:
+// 1 → Circle Area
+// 2 → Square Area
+// 3 → Triangle Area
+let number = 3; 
+switch (number) {
+  case 1:
+    console.log("Circle Area");
+    break;
+  case 2:
+    console.log("Square Area");
+    break;
+  case 3:
+    console.log("Triangle Area");
+    break;
+  default:
+    console.log("Null");
+}
+
+// Q8 Input a language code (en, fr, es, de). Display the language name.
+let language = 'en';  
+switch (language) {
+  case 'en':
+    console.log("English");
+    break;
+  case 'fr':
+    console.log("French");
+    break;
+  case 'es':
+    console.log("Spanish");
+    break;
+  case 'de':
+    console.log("German");
+    break;
+  default:
+    console.log("null Language...!");
+}
+
+                         // Loop
+// let arr = [1,2,3];
+// for(var i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+   
+// }
