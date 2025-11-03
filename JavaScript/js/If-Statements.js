6f71a2f129aec76dc454e8c954b7af9096cd9f36
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-03 12:00:07 +0530

    update

---
+++ JavaScript/js/If-Statements.js
@@ -0,0 +1,33 @@
+// let AGE = 75;
+
+// if(AGE >=18 && AGE<56){
+//     console.log("Eligible for vote");  
+// }
+// else if(AGE>=56 && AGE<75){
+//     console.log("Null for vote");
+// }
+// else{
+//     console.log("Not eligible fot vote");
+// }
+
+// Q1
+let number = 10; 
+
+if (number % 2 === 0) {
+  console.log(number + " is even");
+} else {
+  console.log(number + " is odd");
+}
+
+// Q2
+
+let num = 1
+if (num > 1) {
+    console.log("The number is positive");
+} else if (num < -1) {
+    console.log("The number is negative");
+} else {
+    console.log("The number is zero");
+}
+
+// Q3
