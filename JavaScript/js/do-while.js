18da428341f53973324a6eecaa80e5a4f5dfa769
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-05 12:24:44 +0530

    update

--- JavaScript/js/do-while.js
+++ JavaScript/js/do-while.js
@@ -1,5 +1,16 @@
-let numbere;
-do{
-    number = +prompt('enter your number...');
-} while (number >= 0);
-console.log('you enter the negative number?...');
+// let numbere;
+// do{
+//     number = +prompt('enter your number...');
+// } while (number >= 0);
+// console.log('you enter the negative number?...');
+
+
+// Q1 print number from 1 to 10 using a do..while loop
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
+
+
+Q2 
