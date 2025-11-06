1a628d34ff03d7f7fb5c7b0aad1e94ef90cff884
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-06 12:24:44 +0530

    update

--- JavaScript/js/function.js
+++ JavaScript/js/function.js
@@ -9,9 +9,61 @@ let n8 = 8;
 let names = 'Rishabh Yadav..';
 
 function name(a, b, c, d, e, f, g, i, naam) {
-    console.log(a,b,c,d,e,f,g,i);
+    // lexical scop
+    console.log(a, b, c, d, e, f, g, i);
     return[n1+ n2+ n3+ n4+ n5+ n6+ n7+ n8,naam];
     
 }
 
-let ans =name(n1, n2, n3, n4, n5, n6, n7, n8, names);
+let ans = name(n1, n2, n3, n4, n5, n6, n7, n8, names);
+console.log(ans);
+
+
+
+        //                              Calculator Projects using Function
+        // function name -> cal -> switch
+        // user -> number1, number2 ,operator(+, -, * , /, %)
+       
+
+function cal(number1, number2, operator) {
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = number1 + number2;
+            break;
+
+        case '-':
+            result = number1 - number2;
+            break;
+
+        case '*':
+            result = number1 * number2;
+            break;
+
+        case '/':
+            if (number2 === 0) {
+                return " Division by zero is not allowed.";
+            }
+            result = number1 / number2;
+            break;
+
+        case '%':
+            result = number1 % number2;
+            break;
+
+        default:
+            return "Invalid operator! Please use +, -, *, /, or %.";
+    }
+
+    return `Result: ${result}`;
+}
+
+
+console.log(cal(10, 5, '+'));  
+console.log(cal(10, 9, '-'));  
+console.log(cal(10, 12, '*'));
+console.log(cal(10, 2, '/'));
+console.log(cal(55, 8, '%'));
+
+
