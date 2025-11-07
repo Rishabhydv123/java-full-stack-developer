205abd52997c780e96914a3e23ab168e3ed6070b
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-07 21:48:06 +0530

    update

--- JavaScript/js/do-while.js
+++ JavaScript/js/do-while.js
@@ -6,11 +6,21 @@
 
 
 // Q1 print number from 1 to 10 using a do..while loop
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+
+
+// Q2 print all even numbers between 1 and 50.
 let i = 1;
+
 do {
-  console.log(i);
+  if (i % 2 === 0) {
+    console.log(i);
+  }
   i++;
-} while (i <= 10);
+} while (i <= 50);
 
-
-Q2 
+// Q3 Print all odd numbers between 1 and 50.
