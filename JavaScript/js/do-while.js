7fcd15dd110d0a0df107d2cf573adae59c2a615b
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-08 19:26:21 +0530

    update

--- JavaScript/js/do-while.js
+++ JavaScript/js/do-while.js
@@ -24,13 +24,13 @@
 // } while (i <= 50);
 
 // Q3 Print all odd numbers between 1 and 50.
-// let i = 1;
+let i = 1;
 
-// do {
-//   if (i % 2 !== 0) {
-//     console.log(i);
-//   }
-//   i++;
-// } while (i <= 50);
+do {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+  i++;
+} while (i <= 50);
 
 Q3 
