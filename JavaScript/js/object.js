56aa873f7fad14dfb4475ee5f3eac4f48accb145
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-10 12:16:05 +0530

    code of the day

---
+++ JavaScript/js/object.js
@@ -0,0 +1,19 @@
+// object => document => object => literal
+// {key:value} pair
+
+let object = {
+    name: 'Rishabh Yadav',
+    age: 21,
+    maried:false,
+    hobbies:['pub-G', 'coc', 'cs'],
+    school: {
+        Graduation:'jmpc',
+        XII: 'jmpc',
+        X: 'pvm',
+    },
+};
+
+console.log(object.name);
+console.log(object.age);
+console.log(object.maried);
+console.log(object.hobbies);
