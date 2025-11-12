03a483a533977a1cf0ceef16cfa143488edd5e87
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-12 12:44:28 +0530

    update

--- JavaScript/js/Array-Map.js
+++ JavaScript/js/Array-Map.js
@@ -2,13 +2,21 @@ let number = 'helloo';
 
 // Create an array of indexes + 1
 let newArr = Array.from(number, (_, i) => i + 1);
-console.log(newArr);
+// console.log(newArr);
 
 // Map
 let data = newArr.map((element, index) => {
   return element < 5 ? [index] : null;
 });
-console.log(data);
+// console.log(data);
+
+// TAsk:
+let array1 = [2, 7, 0, 3, 1, 6, 4, 8];
+let newArray1 = array1.map(num => num + -2);
+console.log(newArray1);
+
+
+
 
 // Filter
 let str = ["mango", "apple", "kiwi", "watermelon", "mango", "apple", "kiwi", "banana", "lich", "muskmelon"];
@@ -16,16 +24,34 @@ let str = ["mango", "apple", "kiwi", "watermelon", "mango", "apple", "kiwi", "ba
 let data1 = str.filter((element, index) => {
   return index === 0;
 });
-console.log(data1);
+// console.log(data1);
 
 // forEach
 str.forEach((el, i) => {
-  if (el === "apple") console.log(el, i);
+  // if (el === "apple") console.log(el, i);
 });
 
 
 // Reduce
 
+let data2 = str.reduce((acc, curr) =>{
+  // console.log(curr);
+  acc[curr] = (acc[curr] || 0) +1;
+  return acc;
+}, {});
 
+// console.log(data2);
+
+
+// Task: let array=[2,7,0,3,1,6,4,8] 
+
+let array = [2, 7, 0, 3, 1, 6, 4, 8];
+
+let newArray = array.reduce((acc, curr) => {
+  acc.push(curr * 2);
+  return acc;
+}, []);
+
+console.log(newArray);
 
 
