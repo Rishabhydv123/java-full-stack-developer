7c60f0fd099c56b9ca70ae35a5cd547d4f463fe7
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-19 10:39:30 +0530

    update

--- JavaScript/js/prototypes.js
+++ JavaScript/js/prototypes.js
@@ -30,4 +30,30 @@ console.log("arr1", arr1);
 //  Map
 //  filter
 
-let arr2 = [1,2,3,4];
+let arr2 = [1, 2, 3, 4];
+
+Array.prototype.countElements = function () {
+    let count = 0;
+    this.forEach(() => count++);
+    return count;
+};
+
+let ans1 = arr2.countElements();
+console.log("countElements:", ans1);
+
+// Custom pop-like method
+Array.prototype.yes = function () {
+    let last_index = this.length - 1;
+
+    delete this[last_index];   // removes last element but keeps a hole
+    console.log("arr2 after delete:", this);
+
+    // remove empty slots
+    return this.filter(el => el != null);
+};
+
+arr2 = arr2.yes();
+arr2 = arr2.yes();
+let anss = arr2.yes();
+
+console.log("arr2:pop_app", anss);
