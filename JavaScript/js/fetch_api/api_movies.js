c7219f319d860549a1375994a98471b497dae4ac
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-25 10:42:05 +0530

    update

--- JavaScript/js/fetch_api/api_movies.js
+++ JavaScript/js/fetch_api/api_movies.js
@@ -1,23 +1,25 @@
 const apikey = '67cf508d'
 
 async function apiCall() {
-    const apiurl = 'http://www.omdbapi.com/?apikey=${apikey}&s=${value}'
-    let res = await fetch(API);
+    const apiURL = 'http://www.omdbapi.com/?apikey=${apikey}&s=${value}';
+    let res = await fetch(apiURL);
     let data = await res.json();
     UI = (data.Search);
-}
+};
 
 const inputvalue = () => {
-    let inputvalue = document.querySelector('#input').ariaValueMax;
+    let inputvalue = document.querySelector('#input').value;
     apiCall(inputvalue);
-}
+};
 
 const UI =(data) => {
     console.log(data);
     const mainDiv = document.querySelector('#container');
 
+    mainDiv.innerHTML ='';
+
     
-    data.map((el, i) =>{
+    data.map((el, i) => {
     let cardDiv = document.createElement("div");
     let img = document.createElement("img");
     let imdbID = document.createElement("p");
@@ -27,14 +29,15 @@ const UI =(data) => {
 
      cardDiv.className = `div_${i+1}`;
 
-
-
-
+    img.src = el.Poster;
+    imdbID.innerText = 'id ${el.imbdID}';
+    Title.innerText = 'title ${el.Title}';
+    Type.innerText = 'type ${el.Type}';
+    Year.innerText = 'year ${el.year}';
 
     cardDiv.append(
         img,imdbID,Title,Type,Year
     );
-
     content.append(cardDiv);
     });
 }
