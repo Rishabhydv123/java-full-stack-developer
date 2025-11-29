b0303d2ca444b6eb50152741c807736b0f33e33d
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-11-29 19:23:02 +0530

    update

---
+++ JavaScript/js/Task/Mode_Saver.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("theme-btn");
+
+document.body.classList.toggle("dark", localStorage.getItem("theme") === "dark");
+
+btn.onclick = () => {
+  document.body.classList.toggle("dark");
+  localStorage.setItem("theme", document.body.classList.contains("dark") ? "dark" : "light");
+};
