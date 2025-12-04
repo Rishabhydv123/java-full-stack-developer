c43d28c3115476c451ab07a0922438a357d5a6ea
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-12-04 21:34:59 +0530

    update

--- json/backend/index.js
+++ json/backend/index.js
@@ -1,1 +1,13 @@
-//npx json-server --watch db.json --port 3001
+// importing json-server library
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 3001; 
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
+// npx json-server --watch db.json --port 3001
+
+
