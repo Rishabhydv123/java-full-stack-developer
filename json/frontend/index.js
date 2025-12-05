e58defcc1065882c05bcb0b833c6cf7a737847f8
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-12-05 10:42:05 +0530

    update

--- json/frontend/index.js
+++ json/frontend/index.js
@@ -1,31 +1,41 @@
-let data;
- const API = `http://localhost:3001/todos`;
+let data = [];
+const API = `http://localhost:3001/todos`;
 
- function myTodosAdd(){
-    const todotext = document.querySelector('#todos').value;
+function myTodosAdd() {
+    const todoText = document.querySelector('#todos').value.trim();
     console.log(todoText);
 
-    console.log(data);
-
-    if (todoText.trim() === '') return;
+    if (todoText === '') return;
 
     let newTodos = {
-        id: +data.length + 1,
+        id: data.length + 1,
         text: todoText,
         isEdit: false,
         isCompleted: false,
     };
 
-    fetch(API,{
-        method: 'post',
-        headers:{
+    fetch(API, {
+        method: 'POST',
+        headers: {
             'content-type': 'application/json',
         },
-        body:JSON.stringify(newTodos),
+        body: JSON.stringify(newTodos),
     })
-    
+        .then(res => res.json())
+        .then(() => {
+            alert('Data has been sent');
+            fetchTodoDB(); 
+        })
+        .catch(err => console.error("Error from addTodo:", err));
+}
 
-
-    
-    
- }
+function fetchTodoDB() {
+    fetch(API)
+        .then(res => res.json())
+        .then(res => {
+            data = [...res];
+            console.log('Fetched data:', data);
+            showTodos(); 
+        })
+        .catch(err => console.error("Fetch error:", err));
+}
