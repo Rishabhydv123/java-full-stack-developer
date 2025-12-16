985cfa1e3c65b4d3d114c676cb576948f0c0d9b3
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-12-16 11:09:23 +0530

    update

--- local storage/componets/navbar.js
+++ local storage/componets/navbar.js
@@ -23,3 +23,27 @@
 // export default { sumOfTwoNumber, subOfTwoNumber };
 
 // default export
+
+
+
+   
+export const navbar = () => {
+  return `
+    <div class="logo">
+      <img src="https://fakestoreapi.com/icons/logo.png" height="60" width="60" alt="Logo">
+    </div>
+
+    <ul>
+      <li>Home</li>
+      <li>Product</li>
+      <li>Contact</li>
+    </ul>
+
+    <ul>
+      <li onclick="window.location='/local storage/page/cart.html'">Cart 🛒</li>
+      <li onclick="window.location='/local storage/page/Login.html'">Login</li>
+      <li onclick="window.location='/local storage/page/Signup.html'">Signup</li>
+    </ul>
+  `;
+};
+
