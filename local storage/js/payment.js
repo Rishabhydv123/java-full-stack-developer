a029ab050dc1fd780f755141cd71f2345ed7d7a5
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-12-17 11:05:59 +0530

    update

--- local storage/js/payment.js
+++ local storage/js/payment.js
@@ -2,10 +2,19 @@
     document.querySelectorAll('.payment-option').forEach(option => {
       option.classList.remove('selected');
     });
+    
 
     element.classList.add('selected');
 
     const continueBtn = document.getElementById('continueBtn');
     continueBtn.disabled = false;
     continueBtn.classList.add('enabled');
-  }
+
+
+}
+
+function continueBtn() {
+  window.location.href = "/local storage/index.html";
+}
+
+ 
