792889850ae6937c5f0471f50e630a8534d797d2
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-12-18 09:49:21 +0530

    update

---
+++ local storage/js/payment.js
@@ -0,0 +1,25 @@
+function selectPayment(element) {
+  document.querySelectorAll(".payment-option").forEach(opt => {
+    opt.classList.remove("selected");
+  });
+
+  element.classList.add("selected");
+
+  const btn = document.getElementById("continueBtn");
+  btn.disabled = false;
+  btn.style.background = "green";
+  btn.style.color = "white";
+}
+
+function completePayment() {
+  alert("Payment Successful ✅");
+
+  
+  localStorage.removeItem("cart");
+
+  window.location.href = "/local storage/index.html";
+}
+
+function goBack() {
+  window.history.back();
+}
