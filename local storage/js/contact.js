528ab1ec9bd8728afcbf06b6be9c482140d72492
Rishabhydv123 <rishabhyadav55888@gmail.com>
2025-12-26 19:23:08 +0530

    code of the day

---
+++ local storage/js/contact.js
@@ -0,0 +1,16 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  if (name === "" || email === "" || message === "") {
+    alert("Please fill all fields");
+  } else {
+    document.getElementById("successMsg").innerText =
+      "Thank you, " + name + "! Your message has been sent.";
+    
+    document.getElementById("contactForm").reset();
+  }
+});
