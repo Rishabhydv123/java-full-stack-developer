b20778ee5e8dfcc1f9ddd61dccd5bf3b16858b37
Rishabhydv123 <rishabhyadav55888@gmail.com>
2026-01-06 09:56:50 +0530

    update

--- Task and Project/javascript/todo.js
+++ Task and Project/javascript/todo.js
@@ -1,42 +1,52 @@
-let store = [];
+let store = JSON.parse(localStorage.getItem("todos")) || [];
+
+const saveToLocalStorage = () => {
+    localStorage.setItem("todos", JSON.stringify(store));
+};
 
 const addTodo = () => {
     const valueText = document.getElementById('todo').value.trim();
-    
 
     if (valueText === "") {
         alert("Please enter a task!");
         return;
     }
 
-    let todoDetails = {
+    const todoDetails = {
         id: Date.now(),
         textTodo: valueText,
-        isEdit: false,
         isCompleted: false,
     };
 
     store.push(todoDetails);
+    saveToLocalStorage();
     renderonUI(store);
+
     document.getElementById('todo').value = "";
 };
 
 const renderonUI = (data) => {
     const mainDiv = document.getElementById('todo_list');
-    mainDiv.innerHTML = '';
+    mainDiv.innerHTML = "";
 
-    
-
-    data.map((el) => {
-
+    data.forEach((el) => {
         const childDiv = document.createElement('div');
 
         const CheckBox = document.createElement('input');
         CheckBox.type = "checkbox";
         CheckBox.checked = el.isCompleted;
 
+        CheckBox.addEventListener('change', () => {
+            el.isCompleted = CheckBox.checked;
+            saveToLocalStorage();
+        });
+
         const text = document.createElement('h3');
         text.innerText = el.textTodo;
+
+        if (el.isCompleted) {
+            text.style.textDecoration = "line-through";
+        }
 
         const btn_edit = document.createElement('button');
         btn_edit.innerText = 'Edit';
@@ -46,27 +56,31 @@ const renderonUI = (data) => {
 
         btn_delete.addEventListener('click', () => {
             store = store.filter((item) => item.id !== el.id);
+            saveToLocalStorage();
             renderonUI(store);
         });
 
         btn_edit.addEventListener('click', () => {
-
             const editInput = document.createElement('input');
             editInput.type = "text";
             editInput.value = el.textTodo;
 
             const btn_save = document.createElement('button');
-            btn_save.innerText = "confirm";
+            btn_save.innerText = "Confirm";
 
             const btn_cancel = document.createElement('button');
             btn_cancel.innerText = "Cancel";
 
-            childDiv.innerHTML = "";  
+            childDiv.innerHTML = "";
             childDiv.append(CheckBox, editInput, btn_save, btn_cancel, btn_delete);
 
             btn_save.addEventListener('click', () => {
-                el.textTodo = editInput.value.trim();
-                renderonUI(store);
+                const updatedText = editInput.value.trim();
+                if (updatedText !== "") {
+                    el.textTodo = updatedText;
+                    saveToLocalStorage();
+                    renderonUI(store);
+                }
             });
 
             btn_cancel.addEventListener('click', () => {
@@ -78,3 +92,7 @@ const renderonUI = (data) => {
         mainDiv.append(childDiv);
     });
 };
+
+document.getElementById('btn').addEventListener('click', addTodo);
+
+renderonUI(store);
