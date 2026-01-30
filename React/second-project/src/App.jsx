5e1f5d5ab7539f64ccae218923edc370376eba5d
Rishabhydv123 <rishabhyadav55888@gmail.com>
2026-01-30 11:01:47 +0530

    new update

--- React/second-project/src/App.jsx
+++ React/second-project/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // logic js syntax
+
+  const data = [
+    "mehfooz",
+    "khan",
+    "nillu",
+    "parru",
+    "shaddu",
+    "rissu",
+    "hummu",
+  ];
+
+  // HOF - [filter, map, reduce, forEach]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* html syntax */}
+
+      {/* 
+      <h1>{data[0]}</h1>
+      <h1>{data[1]}</h1>
+      <h1>{data[2]}</h1>
+      <h1>{data[3]}</h1>
+      <h1>{data[4]}</h1>
+      <h1>{data[5]}</h1>
+      <h1>{data[6]}</h1> 
+      */}
+
+      {data.map((el) => (
+        <h1>{el}</h1>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export {App};
