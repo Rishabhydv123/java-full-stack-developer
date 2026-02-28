03ba39cc60ce0748f3cb844993e4caaf63eb85b9
Rishabhydv123 <rishabhyadav55888@gmail.com>
2026-02-28 11:23:56 +0530

    update

--- React/Routes/src/Pages/login.jsx
+++ React/Routes/src/Pages/login.jsx
@@ -1,29 +1,51 @@
 import React, { useContext, useState } from 'react'
 import { AuthValContext } from '../Context/AuthProvider';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
-    const [email,setEmail] = useState('');
-    const [pass,setPass] = useState('');
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
 
-    const {auth ,setAuth} = useContext(AuthValContext);
+  const { setAuth } = useContext(AuthValContext);
+  const navigate = useNavigate();   
 
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        if(email.trim()=== '' && pass.trim()=== '') return;
-        setAuth(true)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (email.trim() === '' || pass.trim() === '') return;
+
+    setAuth(true);        
+    navigate('/user');  
+  }
+
   return (
     <>
       <h1>Login</h1>
-      <form style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",gap:"1rem"}} 
-      onSubmit={handleSubmit}>
-        <label htmlFor='email'></label>
-        <input type='email' name='email' id='email' onChange={(e)=>setEmail(e.target.value)}></input>
+      <form
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: "1rem"
+        }}
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <label htmlFor='pass'></label>
-        <input type='password' name='pass' id='pass' onChange={(e)=>setPass(e.target.value)}></input>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+        />
 
-        <button type='submit'>submit</button>
+        <button type="submit">Login</button>
       </form>
     </>
   )
